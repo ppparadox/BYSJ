299471e5308f1d97e52c62901ba3ab8aba8c17ec
1-UP <1584824430@qq.com>
2018-05-07 10:17:25 +0800

    debug

--- bisheserver/server.js
+++ bisheserver/server.js
@@ -87,11 +87,7 @@ app.post('/register', function (req, res) {
   });
 
 })
-var server = app.listen(8089, function () {
-  var host = server.address().address
-  var port = server.address().port 
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
-})
+
 
 //公交注册模块
 app.post('/busregister', function (req, res) {
